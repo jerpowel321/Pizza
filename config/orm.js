fb05ca41cf4a2ba8ade55e3cfd5e8a2c5e8b3b9a
Jennifer Powell <jerpowel321@gmail.com>
2019-04-04 23:28:26 -0700

    initial file setup & schema & seed data

---
+++ config/orm.js
@@ -0,0 +1,7 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+// Export the orm object.
+module.exports = orm;
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO pizzas (name) VALUES ('Pepperoni');
+INSERT INTO pizzas (name) VALUES ('Classic Cheese');
+INSERT INTO pizzas (name) VALUES ('Hawaiian');
+INSERT INTO pizzas (name) VALUES ('Meat Combo');
+INSERT INTO pizzas (name) VALUES ('Chicken Pesto');
